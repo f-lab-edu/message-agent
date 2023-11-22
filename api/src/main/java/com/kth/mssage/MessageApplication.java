b08c8a213739591923973f43741fafa51964200e
KTH-96 <taehyun960@gmail.com>
2023-11-22 16:46:53 +0900

    feat 날씨 정보를 text mesaage로 가공하는 service

--- api/src/main/java/com/kth/mssage/MessageApplication.java
+++ api/src/main/java/com/kth/mssage/MessageApplication.java
@@ -2,7 +2,11 @@ package com.kth.mssage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@ConfigurationPropertiesScan
+@EnableFeignClients
 @SpringBootApplication
 public class MessageApplication {
     public static void main(String[] args) {
