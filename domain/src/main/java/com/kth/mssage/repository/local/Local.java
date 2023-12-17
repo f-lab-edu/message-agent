36d87fd115cc2504c20585d12bd1b0e91ccf0fa6
KTH-96 <taehyun960@gmail.com>
2023-12-17 17:24:51 +0900

    feat: 지역별 날씨에 대한 entity 및 repository 생성

---
+++ domain/src/main/java/com/kth/mssage/repository/local/Local.java
@@ -0,0 +1,48 @@
+package com.kth.mssage.repository.local;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "region_city")
+    private String regionCity;
+
+    @Column(name = "region_town")
+    private String regionTown;
+
+    @Column(name = "region_village")
+    private String regionVillage;
+
+    @Column(name = "nx", nullable = false)
+    private  String nx;
+
+    @Column(name = "ny", nullable = false)
+    private  String ny;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Local that = (Local) o;
+
+        String thisConcatenated = regionCity + " " + regionTown + " " + regionVillage;
+        String thatConcatenated = that.regionCity + " " + that.regionTown + " " + that.regionVillage;
+
+        return thisConcatenated.equals(thatConcatenated);
+    }
+
+    @Override
+    public int hashCode() {
+        return (regionCity + " " + regionTown + " " + regionVillage).hashCode();
+    }
+}
---
+++ domain/src/main/java/com/kth/mssage/repository/local/LocalRepository.java
@@ -0,0 +1,7 @@
+package com.kth.mssage.repository.local;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocalRepository extends JpaRepository<Local, Long> {
+
+}
