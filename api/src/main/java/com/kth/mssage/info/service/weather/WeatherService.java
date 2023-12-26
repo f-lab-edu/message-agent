8a0f23fdfe492570d7e91bdca94435fcc3bac504
KTH-96 <taehyun960@gmail.com>
2023-12-26 15:37:27 +0900

    fix: 테스트시 인메모리 디비 사용할 수 있도록 변경

--- api/src/main/java/com/kth/mssage/info/service/weather/WeatherService.java
+++ api/src/main/java/com/kth/mssage/info/service/weather/WeatherService.java
@@ -4,25 +4,16 @@ import com.kth.mssage.info.client.WeatherApiClient;
 import com.kth.mssage.info.properties.WeatherProperties;
 import com.kth.mssage.info.web.dto.info.WeatherInfoDto;
 import com.kth.mssage.info.web.dto.request.skill.WeatherDto;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.kth.mssage.repository.local.Geometry;
+import com.kth.mssage.repository.local.GeometryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -34,13 +25,14 @@ public class WeatherService {
 
     private final WeatherProperties weatherProperties;
     private final WeatherApiClient weatherApiClient;
+    private final GeometryRepository geometryRepository;
 
     public WeatherInfoDto getWeatherInfoDto(WeatherDto weatherDto) {
         String location = weatherDto.getLocation();
 
-        LocalInfoValue localInfoValue = findByRegion(location);
+        Geometry geometry = findByGeometryByLocation(location);
 
-        String weatherInfo = getWeatherInfo(localInfoValue.getNx(), localInfoValue.getNy());
+        String weatherInfo = getWeatherInfo(geometry.getNx(), geometry.getNy());
         log.info("날씨 Json data "+ weatherInfo);
 
         JSONObject jsonObject = new JSONObject(weatherInfo);
@@ -68,41 +60,15 @@ public class WeatherService {
         return new WeatherInfoDto(location, temp, rainAmount, humid);
     }
 
-    public LocalInfoValue findByRegion(String location) {
+    public Geometry findByGeometryByLocation(String location) {
         String[] parts = location.split(" ", 3);
 
         String regionCity = parts.length > 0 ? parts[0] : "";
         String regionTown = parts.length > 1  ? parts[1] : "";
         String regionVillage = parts.length > 2 ? parts[2] : "";
 
-        return loadSingleWeatherInfo(new LocalInfoKey(regionCity, regionTown, regionVillage));
-    }
 
-    @Cacheable(value = "weatherInfo", key = "#localInfoKey")
-    public LocalInfoValue loadSingleWeatherInfo(LocalInfoKey localInfoKey) {
-        Map<LocalInfoKey, LocalInfoValue> localInfoMap = loadWeatherInfo();
-        return localInfoMap.get(localInfoKey);
-    }
-
-    @Cacheable(value = "weatherInfo")
-    public Map<LocalInfoKey, LocalInfoValue> loadWeatherInfo() {
-        Map<LocalInfoKey, LocalInfoValue> localInfoMap = new HashMap<>();
-        try (InputStream inputStream = new ClassPathResource("weather.csv").getInputStream();
-            Reader reader = new InputStreamReader(inputStream);
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
-
-            for (CSVRecord record : csvParser) {
-                localInfoMap.put(
-                    new LocalInfoKey(record.get("region_city"), record.get("region_town"), record.get("region_village")),
-                    new LocalInfoValue(record.get("nx"), record.get("ny"))
-                );
-            }
-        } catch (Exception e) {
-            //TODO: 예외 처리 로직
-        }
-
-
-        return localInfoMap;
+        return geometryRepository.findByRegionCityAndRegionTownAndRegionVillage(regionCity, regionTown, regionVillage);
     }
 
     private String getWeatherInfo(String nx, String ny) {
@@ -126,34 +92,4 @@ public class WeatherService {
         return now.minusHours(now.getMinute() < 40 ? 1 : 0).format(DateTimeFormatter.ofPattern(BASE_TIME));
     }
 
-    @AllArgsConstructor
-    class LocalInfoKey {
-        private final String regionCity;
-        private final String regionTown;
-        private final String regionVillage;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            LocalInfoKey that = (LocalInfoKey) o;
-
-            String thisConcatenated = regionCity + " " + regionTown + " " + regionVillage;
-            String thatConcatenated = that.regionCity + " " + that.regionTown + " " + that.regionVillage;
-
-            return thisConcatenated.equals(thatConcatenated);
-        }
-
-        @Override
-        public int hashCode() {
-            return (regionCity + " " + regionTown + " " + regionVillage).hashCode();
-        }
-    }
-
-    @Getter
-    @AllArgsConstructor
-    class LocalInfoValue {
-        private final String nx;
-        private final String ny;
-    }
 }
--- domain/src/main/java/com/kth/mssage/repository/local/GeometryRepository.java
+++ domain/src/main/java/com/kth/mssage/repository/local/GeometryRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GeometryRepository extends JpaRepository<Geometry, Long> {
 
+    Geometry findByRegionCityAndRegionTownAndRegionVillage(String regionCity, String regionTown, String regionVillage);
 }
