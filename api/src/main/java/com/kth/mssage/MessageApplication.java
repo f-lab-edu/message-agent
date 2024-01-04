540a55f8f43fc874b7a4df5a83654e4d6965d214
KTH-96 <taehyun960@gmail.com>
2024-01-04 23:05:01 +0900

    CSV 파일 배치 적용 (#7)
    * feat: 지역별 날씨에 대한 entity 및 repository 생성
    
    * feat: 날씨 정보로 배치사용해서 DB에 저장
    
    * build: jpa 빌드 및 DB정보 추가
    
    * fix: lombok 및 entity클래스 찾지 못하는 부분 수정
    
    * fix: 애플리케이션 시작시 batch가 시작하지 않는 문제 수정
    
    * refactor: 클래스 이름 변경 및 hashcode, equals 메소드 로직 변경
    
    * fix: 테스트시 인메모리 디비 사용할 수 있도록 변경
    
    * refactor: github action에서 파일 경로를 인식 못하는 부분 변경
    
    * refactor: github action yml에 secrets 변수 넣어줄  파일 경로 수정
    
    - 아직 main 브랜치에 병합이 안되서 파일을 못찾음
    
    * refactor: setter추가 및 완료된 스텝 재등록 할 수 있게 변경
    
    * build: main 브랜치에 push할때 서버에 접속해 도커 파일로 빌드
    
    * fix: 도커 이미지 build, push하는 부분 main브랜치에 push될때 실행하게 변경
    
    * build: github action yml 파일 ci/cd 용도로 나누기

--- api/src/main/java/com/kth/mssage/MessageApplication.java
+++ api/src/main/java/com/kth/mssage/MessageApplication.java
@@ -2,16 +2,20 @@ package com.kth.mssage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableCaching
 @ConfigurationPropertiesScan
 @EnableFeignClients
-@SpringBootApplication
+@EntityScan(basePackages = {"com.kth.mssage.repository"})
+@EnableJpaRepositories(basePackages = "com.kth.mssage.repository")
+@SpringBootApplication(scanBasePackages = {"com.kth"})
 public class MessageApplication {
     public static void main(String[] args) {
         SpringApplication.run(MessageApplication.class, args);
--- api/src/test/java/com/kth/mssage/MessageApplicationTest.java
+++ api/src/test/java/com/kth/mssage/MessageApplicationTest.java
@@ -1,7 +1,9 @@
 package com.kth.mssage;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MessageApplicationTest {
 
