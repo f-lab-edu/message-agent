5bbc6b6fc395b1f430697b4c3e67c423d1a24e8a
KTH-96 <taehyun960@gmail.com>
2024-02-09 14:40:35 +0900

    refactor: domain, infra 모듈 삭제

--- domain/src/main/java/com/kth/mssage/repository/local/GeometryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kth.mssage.repository.local;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeometryRepository extends JpaRepository<Geometry, Long> {
-
-    Geometry findByRegionCityAndRegionTownAndRegionVillage(String regionCity, String regionTown, String regionVillage);
-}
